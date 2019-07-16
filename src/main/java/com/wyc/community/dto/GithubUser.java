0988b6b6cf7fb358473943ae075d7c622862fd32
584491394 <584419394@qq.com>
2019-07-17 01:20:32 +0800

    7.17-1:20

---
+++ src/main/java/com/wyc/community/dto/GithubUser.java
@@ -0,0 +1,15 @@
+package com.wyc.community.dto;
+
+import lombok.Data;
+
+/**
+ * @author 武应琛
+ * @date 2019/7/17
+ */
+
+@Data
+public class GithubUser {
+    private String name;
+    private long id;
+    private String bio;
+}
